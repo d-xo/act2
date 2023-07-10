abf5f1e1e89eab62a822d1de9920dde672b3da8c
David Terry <me@xwvvvvwx.com>
2023-07-10 13:37:13 +0200

    playing around with some ast stuff

--- src/Act/Syntax/Typed.hs
+++ src/Act/Syntax/Typed.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NoFieldSelectors #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Act.Syntax.Typed where
 
@@ -12,15 +16,18 @@ import Prelude hiding (LT, GT)
 
 import Data.Text (Text)
 import Data.Sequence (Seq(..))
+import Data.Hashable
 import Data.HashMap (Map)
 import Data.HashSet (Set)
 import Data.HashSet qualified as Set
 import Data.Parameterized.Some (Some(..))
-import Data.Parameterized.Classes (ShowF, TestEquality(..))
+import Data.Parameterized.Classes
+import Data.Parameterized.TH.GADT
 import Data.Typeable
+import GHC.Generics hiding (Constructor)
 
 
---- Contracts --------------------------------------------------------------------------------------
+-- * Contracts
 
 
 -- | Top level type representing a full specification of a contract
@@ -28,13 +35,14 @@ data Contract = Contract
   { name :: Text
   , storage :: StorageLayout
   , invariants :: Set Invariant
+  , relations :: Set Relation
   , constructor :: Constructor
   , methods :: Set Method
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
 
 
---- Behaviours -------------------------------------------------------------------------------------
+-- * Behaviours
 
 
 -- | Initcode spec
@@ -42,7 +50,7 @@ data Constructor = Constructor
   { params :: Seq (Some (Binder Unquantified))
   , branches :: Cases CBranch
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
 
 -- | A description of a single branch of a constructor execution
 data CBranch = CBranch
@@ -50,7 +58,7 @@ data CBranch = CBranch
   , rewrites :: Set (Some (Update Unquantified))
   , ensures :: Set (Expr Unquantified Timed ABool)
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
 
 -- | Method spec
 data Method where
@@ -74,12 +82,13 @@ data Branch (a :: Ty) = Branch
   , returns :: Expr Unquantified Timed a
   , ensures :: Set (Expr Unquantified Timed ABool)
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
 
 -- | Potentially multiple execution branches
 data Cases s
   = Cases (Map (Expr Unquantified Timed ABool) (Cases s)) s
   | Single s
+  deriving (Generic)
 
 deriving instance (Show s) => Show (Cases s)
 deriving instance (Eq s) => Eq (Cases s)
@@ -87,25 +96,24 @@ deriving instance (Eq s) => Eq (Cases s)
 -- | Introduce a new name
 data Binder (q :: Quantity) (a :: Ty) where
   Binder ::
-    { name :: Text
-    , quantifier :: Quantifier q
+    { quantifier :: Quantifier q
+    , name :: Text
     , ty :: STy a
     } -> Binder q a
 
 deriving instance (ShowF (Binder q))
 deriving instance (Show (Binder q t))
 deriving instance (Eq (Binder q t))
-
 instance TestEquality (Binder q) where
   testEquality (Binder _ _ r) (Binder _ _ r1) = testEquality r r1
 
 
---- Storage ----------------------------------------------------------------------------------------
+-- * Storage
 
 
 -- | Layout of variables in storage
 newtype StorageLayout = StorageLayout (Seq (Some StorageLoc))
-  deriving (Eq, Show)
+  deriving newtype (Eq, Show)
 
 -- | Rewrite loc into exp
 data Update (q :: Quantity) (a :: Ty) where
@@ -117,9 +125,9 @@ data Update (q :: Quantity) (a :: Ty) where
 deriving instance ShowF (Update q)
 deriving instance Show (Update q t)
 deriving instance Eq (Update q t)
-
 instance TestEquality (Update q) where
   testEquality (Rewrite a _) (Rewrite a1 _) = testEquality a a1
+
 
 -- | A pointer to a location in storage
 data StorageLoc (a :: Ty) where
@@ -132,7 +140,6 @@ data StorageLoc (a :: Ty) where
 deriving instance (ShowF StorageLoc)
 deriving instance (Show (StorageLoc t))
 deriving instance (Eq (StorageLoc t))
-
 instance TestEquality StorageLoc where
   testEquality (StorageLoc _ _ r0) (StorageLoc _ _ r1) = testEquality r0 r1
 
@@ -171,8 +178,17 @@ instance TestEquality Time where
   testEquality _ _ = Nothing
 
 
---- Invariants -------------------------------------------------------------------------------------
+-- * Invariants / Relations
 
+
+-- | A contract level relation
+data Relation = Relation
+  { name :: Text
+  , prover :: Prover
+  , vars :: Seq (Some (Binder Quantified))
+  , prop :: Expr Quantified Timed ABool
+  }
+  deriving (Show, Eq, Generic)
 
 -- | A contract level invariant
 data Invariant = Invariant
@@ -181,54 +197,53 @@ data Invariant = Invariant
   , vars :: Seq (Some (Binder Quantified))
   , prop :: Expr Quantified Untimed ABool
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
 
 -- | Supported invariant proof methods
 data Prover
   = SMT
   | Coq
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic, Hashable)
 
 -- | Variables in invariants are quantified
 data Quantity
   = Quantified
   | Unquantified
-  deriving (Show, Eq, Typeable)
+  deriving (Show, Eq, Generic, Hashable)
 
 data Quantifier (q :: Quantity) where
   Forall :: Quantifier Quantified
   Exists :: Quantifier Quantified
-  Neither :: Quantifier Unquantified
+  X      :: Quantifier Unquantified
 
 deriving instance Eq (Quantifier q)
 deriving instance Show (Quantifier q)
+instance TestEquality Quantifier where
+  testEquality Forall Forall = Just Refl
+  testEquality Forall Exists = Just Refl
+  testEquality Exists Forall = Just Refl
+  testEquality X X = Just Refl
+  testEquality _ _ = Nothing
 
 
---- Expressions ------------------------------------------------------------------------------------
+-- * Expressions
 
 
--- | Kind for Expr types
-data Ty
-  = AInt
-  | ABool
-  | AStr
-  deriving Typeable
+{-|
+  Symbolic Computations
 
--- | Existential wrapper so we can put this in lists
-data WExpr (q :: Quantity)(t :: Timing) where
-  WExpr :: STy a -> Expr q t a -> WExpr q t
+  There are different restrictions on allowable expressions in different parts of the syntax tree. The type parameters allow us to make constructing instances that violate these restrictions a type error.
 
-deriving instance Show (WExpr q t)
-instance Eq (WExpr q t) where
-  (WExpr a b) == (WExpr a1 b1) = case testEquality a a1 of
-    Just Refl -> b == b1
-    Nothing -> False
+  - Quantity: Whether variables should be quantified or not
+  - Timing: Whether storage references should refer explicitly to the pre/post state
+  - Ty: The type of the value described by the expression
 
--- | Expressions
-data Expr (q :: Quantity)(t :: Timing) (a :: Ty) where
+  This approach allows us to
+-}
+data Expr (q :: Quantity) (t :: Timing) (a :: Ty) where
   -- Variables and literals
   Lit :: Val a -> Expr q t a
-  Var :: STy a -> Text -> Expr q t a
+  Var :: Quantifier q -> Text -> STy ty -> Expr q t a
   Read :: StorageItem q t a -> Expr q t a
 
   -- Integer
@@ -264,40 +279,12 @@ data Expr (q :: Quantity)(t :: Timing) (a :: Ty) where
 deriving instance ShowF (Expr q t)
 deriving instance Show (Expr q t a)
 deriving instance Typeable (Expr q t a)
-
-data Val (a :: Ty) where
-  VInt :: Integer -> Val AInt
-  VBool :: Bool -> Val ABool
-  VStr :: Text -> Val ABool
-
-deriving instance Show (Val a)
-deriving instance Eq (Val a)
-instance TestEquality Val where
-  testEquality (VInt _) (VInt _) = Just Refl
-  testEquality (VBool _) (VBool _) = Just Refl
-  testEquality (VStr _) (VStr _) = Just Refl
-  testEquality _ _ = Nothing
-
--- | Singleton for Ty
-data STy (a :: Ty) where
-  SInt  :: STy AInt
-  SBool :: STy ABool
-  SStr  :: STy AStr
-
-deriving instance ShowF STy
-deriving instance Show (STy t)
-deriving instance Eq (STy a)
-
-instance TestEquality STy where
-  testEquality SInt SInt = Just Refl
-  testEquality SBool SBool = Just Refl
-  testEquality SStr SStr = Just Refl
-  testEquality _ _ = Nothing
-
 instance Eq (Expr q t a) where
   Lit a == Lit a1 = a == a1
-  Var a b == Var a1 b1 = case testEquality a a1 of
-    Just Refl -> b == b1
+  Var a b c == Var a1 b1 c1 = case testEquality a a1 of
+    Just Refl -> case testEquality c c1 of
+      Just Refl -> b == b1
+      Nothing -> False
     Nothing -> False
   Read a == Read b = a == b
   Add a b == Add a1 b1 = a == a1 && b == b1
@@ -321,6 +308,103 @@ instance Eq (Expr q t a) where
     Nothing -> False
   Fin a b c == Fin a1 b1 c1 = a == a1 && b == b1 && c == c1
   _ == _ = False
+
+
+-- ** Auxiliary Types
+
+
+-- | Kind for Expr types
+data Ty
+  = AInt
+  | ABool
+  | AStr
+
+-- | Value wrapper type to allow for a polymorphic Lit
+data Val (a :: Ty) where
+  VInt :: Integer -> Val AInt
+  VBool :: Bool -> Val ABool
+  VStr :: Text -> Val ABool
+
+deriving instance Show (Val a)
+deriving instance Eq (Val a)
+instance TestEquality Val where
+  testEquality (VInt _) (VInt _) = Just Refl
+  testEquality (VBool _) (VBool _) = Just Refl
+  testEquality (VStr _) (VStr _) = Just Refl
+  testEquality _ _ = Nothing
+
+-- | Singleton for Ty
+data STy (a :: Ty) where
+  SInt  :: STy AInt
+  SBool :: STy ABool
+  SStr  :: STy AStr
+
+deriving instance ShowF STy
+deriving instance Show (STy t)
+deriving instance Eq (STy a)
+instance TestEquality STy where
+  testEquality SInt SInt = Just Refl
+  testEquality SBool SBool = Just Refl
+  testEquality SStr SStr = Just Refl
+  testEquality _ _ = Nothing
+
+-- | Existential wrapper so we can put (Expr q t) into lists
+data WExpr (q :: Quantity)(t :: Timing) where
+  WExpr :: STy a -> Expr q t a -> WExpr q t
+
+deriving instance Show (WExpr q t)
+instance Eq (WExpr q t) where
+  (WExpr a b) == (WExpr a1 b1) = case testEquality a a1 of
+    Just Refl -> b == b1
+    Nothing -> False
+
+
+-- * Hashable Instances
+
+
+-- We have to do these instances all at once underneath this top level
+-- splice because of the way that template haskell splices affect mutual
+-- recursion within a file. see: https://blog.monadfix.com/th-groups
+$(return[])
+
+deriving instance Hashable Invariant
+deriving instance Hashable Constructor
+deriving instance Hashable CBranch
+deriving instance Hashable Contract
+deriving instance Hashable Relation
+deriving newtype instance Hashable StorageLayout
+deriving instance Hashable s => Hashable (Cases s)
+
+instance Hashable (Branch a) where
+  hashWithSalt = $(structuralHashWithSalt [t|Branch|] [])
+instance Hashable Method where
+  hashWithSalt = $(structuralHashWithSalt [t|Method|] [])
+instance Hashable (STy a) where
+  hashWithSalt = $(structuralHashWithSalt [t|STy|] [])
+instance HashableF STy where
+  hashWithSaltF = $(structuralHashWithSalt [t|STy|] [])
+instance HashableF (Update q) where
+  hashWithSaltF = $(structuralHashWithSalt [t|Update|] [])
+instance Hashable (Quantifier q) where
+  hashWithSalt = $(structuralHashWithSalt [t|Quantifier|] [])
+instance HashableF (Binder q) where
+  hashWithSaltF = $(structuralHashWithSalt [t|Binder|] [])
+instance Hashable (Expr q t a) where
+  hashWithSalt = $(structuralHashWithSalt [t|Expr|] [])
+instance HashableF (Expr q t) where
+  hashWithSaltF = $(structuralHashWithSalt [t|Expr|] [])
+instance Hashable (StorageItem q t a) where
+  hashWithSalt = $(structuralHashWithSalt [t|StorageItem|] [])
+instance Hashable (StorageLoc a) where
+  hashWithSalt = $(structuralHashWithSalt [t|StorageLoc|] [])
+instance HashableF StorageLoc where
+  hashWithSaltF = $(structuralHashWithSalt [t|StorageLoc|] [])
+instance Hashable (WExpr q t) where
+  hashWithSalt = $(structuralHashWithSalt [t|WExpr|] [])
+instance Hashable (Val a) where
+  hashWithSalt = $(structuralHashWithSalt [t|Val|] [])
+instance Hashable (Time t) where
+  hashWithSalt = $(structuralHashWithSalt [t|Time|] [])
 
 
 --- ERC20 ---
@@ -352,18 +436,30 @@ invariant :: Invariant
 invariant = Invariant
   { name = "total_sum_balances"
   , prover = Coq
-  , vars = [Some (Binder "a" Forall SInt)]
+  , vars = [Some (Binder Forall "a" SInt)]
   , prop = Eq SInt
-      (Sum (StorageItem Whenever balanceOf [WExpr SInt (Var SInt "a")]))
+      (Sum (StorageItem Whenever balanceOf [WExpr SInt (Var Forall "a" SInt)]))
       (Read (StorageItem Whenever totalSupply []))
+  }
+
+relation :: Relation
+relation = Relation
+  { name = "no_inflation"
+  , prover = Coq
+  , vars = [Some (Binder Forall "a" SInt)]
+  , prop = Eq SInt
+      (Read (StorageItem Pre (StorageLoc "totalSupply" [] SInt) []))
+      (Read (StorageItem Post (StorageLoc "totalSupply" [] SInt) []))
   }
 
 constructor :: Constructor
 constructor = Constructor
-  { params = []
+  { params = [Some (Binder X "_name" SStr), Some (Binder X "_symbol" SStr), Some (Binder X "supply" SInt)]
   , branches = Single $ CBranch
       { requires = Set.empty
-      , rewrites = Set.empty
+      , rewrites = Set.fromList
+          [
+          ]
       , ensures = Set.empty
       }
   }
@@ -372,7 +468,8 @@ erc20 :: Contract
 erc20 = Contract
   { name = "ERC20"
   , storage = storage
-  , invariants = undefined
+  , invariants = Set.singleton invariant
+  , relations = Set.singleton relation
   , constructor = undefined
   , methods = undefined
   }
--- token.act.md
+++ token.act.md
@@ -1,101 +1,67 @@
-contract ERC20 {
+contract ERC20:
 
   -- storage layout --
 
-  storage {
-    name        : str
-    symbol      : str
-    totalSupply : u256
-    balanceOf   : addr -> u256
-    allowance   : (addr, addr) -> u256
-  }
+  storage:
+    pub name        : str
+    pub symbol      : str
+    pub totalSupply : u256
+    pub balanceOf   : addr -> u256
+    pub allowance   : (addr, addr) -> u256
 
   -- invariants --
 
   inv @coq total_sum_balances
     = ∀ (a : addr) . sum([balanceOf(a)]) = [totalSupply]
 
+  rel @smt no_inflation
+    = ∀ (a : addr) . [totalSupply] = [totalSupply]!
+
   -- initializiation --
 
-  fn constructor(_name : str, _symbol : str, supply : u256) {
-    rewrites {
-      [name]' <- _name
-      [symbol]' <- _symbol
-      [totalSupply]' <- supply
-      [balanceOf(CALLER)]' <- supply
-    }
-  }
+  constructor(_name : str, _symbol : str, supply : u256):
+    rewrites:
+      [name]! <- _name
+      [symbol]! <- _symbol
+      [totalSupply]! <- supply
+      [balanceOf(CALLER)]! <- supply
 
   -- mutators --
 
-  fn approve(usr : addr, amt : u256) -> bool {
-    rewrites [allowance(CALLER,addr)]' <- amt
+  fn approve(usr : addr, amt : u256) -> bool:
+    rewrites [allowance(CALLER,addr)]! <- amt
     returns true
-  }
 
   fn transfer(to : addr, amt : u256) -> bool:
-    case (CALLER /= to) {
-      requires in u256
+    case (CALLER /= to):
+      requires in u256:
         [balanceOf(CALLER)] - amt
         [balanceOf(to)] + amt
       rewrites move(CALLER, to, amt)
       returns true
-    }
-    otherwise {
+
+    otherwise:
       requires in u256 ([balanceOf(CALLER)] - amt)
       returns true
-    }
-  }
 
-  fn transferFrom(src : addr, dst : addr, amt : u256) -> bool {
-    case (src /= dst && src /= CALLER) {
-      requires in u256
+  fn transferFrom(src : addr, dst : addr, amt : u256) -> bool:
+    case (src /= dst && src /= CALLER):
+      requires in u256:
         [balanceOf(src)] - amt
         [balanceOf(dst)] + amt
         [allowance(src, caller)] - amt
 
-      rewrites
+      rewrites:
         [allowance(src, CALLER)]! <- [allowance(src, CALLER)] - amt
         move(from, to, amount)
 
       returns true
-    }
-    otherwise {
+
+    otherwise:
       requires in u256 ([balanceOf(src)] - amt)
       returns true
-    }
-  }
 
-  -- state getters --
-
-  fn allowance(src : addr, dst : addr) -> u256 {
-    returns [allowance(src, dst)]
-  }
-
-  fn balanceOf(usr : addr) -> u256 {
-    returns [balanceOf(usr)]
-  }
-
-  fn totalSupply() -> u256 {
-    returns [totalSupply]
-  }
-
-  -- metadata --
-
-  fn name() -> str {
-    returns [name]
-  }
-
-  fn symbol() -> str {
-    returns [symbol]
-  }
-
-  fn decimals() -> str {
-    returns 18
-  }
-} where {
-  move = macro!(src : addr, dst : addr, amt : u256) -> rewrites {
+where
+  move = macro!(src : addr, dst : addr, amt : u256) -> rewrites:
     [balanceOf(from)]! <- [balanceOf(from)] - amt
     [balanceOf(to)]!   <- [balanceOf(to)] + amt
-  }
-}
